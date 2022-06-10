0b5d91ff5030744f51ea4a17f2883f8f3354a5f4
Fabio Costa <fabio.costa89@gmail.com>
2022-06-10 12:30:47 +0000

    React version from .env file instead of package.json

--- src/redirect-me/src/components/footer/Footer.tsx
+++ src/redirect-me/src/components/footer/Footer.tsx
@@ -4,7 +4,6 @@ import Container from '@material-ui/core/Container';
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
-import packageJson from '../../../package.json';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -43,7 +42,7 @@ const Footer = () => {
                 >Source Code
               </Link>
               <span> | </span>
-              <span>v{packageJson.version}</span>
+              <span>v{process.env.REACT_APP_VERSION}</span>
             </Typography>
           </Toolbar>
         </Container>
